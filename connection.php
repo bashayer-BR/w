847a340744ca208237ebb3a1b794aef18e3335bc
bashayer-BR <85635870+bashayer-BR@users.noreply.github.com>
2024-01-27 15:58:53 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,7 @@
 <?php
 try {
-    $con = new PDO('mysql:host=localhost;dbname=id21372845_laftah','id21372845_laftahbashayer','Bashayer@2024');
+    // $con = new PDO('mysql:host=localhost;dbname=id21372845_laftah','id21372845_laftahbashayer','Bashayer@2024');
+    $con = new PDO('mysql:host=sql6.freesqldatabase.com;dbname=sql6680105','sql6680105','7jmY3hkJyw');
     $con ->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     // echo "yesss connect";
     //code...
